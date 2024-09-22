51b07e52f2fff429e5eb3a4d19b033730e627ae0
Mamoune Bastor <Mamounebastor7@gmail.com>
2024-09-22 03:16:59 +0200

    Creation de lobjet restaurant et dune liste pour les restaurants et l'adapter avec le Spinner

--- app/src/main/java/com/example/travailpratique1_lounibastor/MainActivity.java
+++ app/src/main/java/com/example/travailpratique1_lounibastor/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.travailpratique1_lounibastor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -12,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +39,15 @@ public class MainActivity extends AppCompatActivity {
         spn_nomResto = (Spinner) findViewById(R.id.spn_nomResto);
         tv_placesRestantes = (TextView) findViewById(R.id.tv_placesReserve);
 
+        ArrayList<restaurant> listeRestaurants= new ArrayList<>();
+        listeRestaurants.add(new restaurant(1,"Bnine",15,15));
+        listeRestaurants.add(new restaurant(2,"CHEZ RAMS",10,10));
+        listeRestaurants.add(new restaurant(3,"McHalal",9,9));
+        listeRestaurants.add(new restaurant(4,"Tacos del caso",20,20));
+        listeRestaurants.add(new restaurant(1,"Fruit el Goju",35,35));
+        ArrayAdapter<restaurant> adaptateur = new ArrayAdapter<restaurant>(this, android.R.layout.simple_spinner_item,listeRestaurants);
+        adaptateur.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spn_nomResto.setAdapter(adaptateur);
     }
 
     public void onClick_reserverTable(View view) {
---
+++ app/src/main/java/com/example/travailpratique1_lounibastor/restaurant.java
@@ -0,0 +1,79 @@
+package com.example.travailpratique1_lounibastor;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class restaurant implements Parcelable {
+ private int noRestaurant;
+ private String nomRestaurant;
+ private int nbPlacesMax;
+ private int nbPlacesRestantes;
+
+    public int getNoRestaurant() {
+        return noRestaurant;
+    }
+
+    public void setNoRestaurant(int noRestaurant) {
+        this.noRestaurant = noRestaurant;
+    }
+
+    public String getNomRestaurant() {
+        return nomRestaurant;
+    }
+
+    public void setNomRestaurant(String nomRestaurant) {
+        this.nomRestaurant = nomRestaurant;
+    }
+
+    public int getNbPlacesMax() {
+        return nbPlacesMax;
+    }
+
+    public void setNbPlacesMax(int nbPlacesMax) {
+        this.nbPlacesMax = nbPlacesMax;
+    }
+
+    public int getNbPlacesRestantes() {
+        return nbPlacesRestantes;
+    }
+
+    public void setNbPlacesRestantes(int nbPlacesRestantes) {
+        this.nbPlacesRestantes = nbPlacesRestantes;
+    }
+
+    public restaurant(int noRestaurant, String nomRestaurant, int nbPlacesMax, int nbPlacesRestantes) {
+        this.noRestaurant = noRestaurant;
+        this.nomRestaurant = nomRestaurant;
+        this.nbPlacesMax = nbPlacesMax;
+        this.nbPlacesRestantes = nbPlacesRestantes;
+    }
+
+    protected restaurant(Parcel in) {
+    }
+
+    public static final Creator<restaurant> CREATOR = new Creator<restaurant>() {
+        @Override
+        public restaurant createFromParcel(Parcel in) {
+            return new restaurant(in);
+        }
+
+        @Override
+        public restaurant[] newArray(int size) {
+            return new restaurant[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+
+    }
+
+
+}
