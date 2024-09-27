63a3919d2b943d2d4fb8ad231069dbf923be98e2
Redouane Louni <2183895@cmontmorency.qc.ca>
2024-09-27 08:09:22 -0400

    Création de la classe reservation

---
+++ app/src/main/java/com/example/travailpratique1_lounibastor/reservation.java
@@ -0,0 +1,72 @@
+package com.example.travailpratique1_lounibastor;
+
+import java.util.Date;
+
+public class reservation {
+
+    private int noReservation;
+    private String dateReservation;
+
+    public int getNoReservation() {
+        return noReservation;
+    }
+
+    public void setNoReservation(int noReservation) {
+        this.noReservation = noReservation;
+    }
+
+    public String getDateReservation() {
+        return dateReservation;
+    }
+
+    public void setDateReservation(String dateReservation) {
+        this.dateReservation = dateReservation;
+    }
+
+    public int getNbPlace() {
+        return nbPlace;
+    }
+
+    public void setNbPlace(int nbPlace) {
+        this.nbPlace = nbPlace;
+    }
+
+    public String getBlocReservationDebut() {
+        return blocReservationDebut;
+    }
+
+    public void setBlocReservationDebut(String blocReservationDebut) {
+        this.blocReservationDebut = blocReservationDebut;
+    }
+
+    public String getBlocReservationFin() {
+        return blocReservationFin;
+    }
+
+    public void setBlocReservationFin(String blocReservationFin) {
+        this.blocReservationFin = blocReservationFin;
+    }
+
+    public String getNomPersonne() {
+        return nomPersonne;
+    }
+
+    public void setNomPersonne(String nomPersonne) {
+        this.nomPersonne = nomPersonne;
+    }
+
+    public String getTelPersonne() {
+        return telPersonne;
+    }
+
+    public void setTelPersonne(String telPersonne) {
+        this.telPersonne = telPersonne;
+    }
+
+    private int nbPlace;
+    private String blocReservationDebut;
+    private String blocReservationFin;
+    private String nomPersonne;
+    private String telPersonne;
+
+}
