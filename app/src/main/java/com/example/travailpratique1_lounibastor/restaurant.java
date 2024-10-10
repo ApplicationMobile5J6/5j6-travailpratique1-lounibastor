71e0c4e6c1a3bb91d7ca321e81e4afeef623b395
Redouane Louni <2183895@cmontmorency.qc.ca>
2024-10-10 13:51:56 -0400

    Création d'un attribut dans restaurant qui contient liste reservation

--- app/src/main/java/com/example/travailpratique1_lounibastor/restaurant.java
+++ app/src/main/java/com/example/travailpratique1_lounibastor/restaurant.java
@@ -5,11 +5,22 @@ import android.os.Parcelable;
 
 import androidx.annotation.NonNull;
 
+import java.util.ArrayList;
+
 public class restaurant implements Parcelable {
- private int noRestaurant;
- private String nomRestaurant;
- private int nbPlacesMax;
- private int nbPlacesRestantes;
+    private int noRestaurant;
+    private String nomRestaurant;
+    private int nbPlacesMax;
+    private int nbPlacesRestantes;
+    private ArrayList<reservation> listeReservations;
+
+    public restaurant(int noRestaurant, String nomRestaurant, int nbPlacesMax, int nbPlacesRestantes) {
+        this.noRestaurant = noRestaurant;
+        this.nomRestaurant = nomRestaurant;
+        this.nbPlacesMax = nbPlacesMax;
+        this.nbPlacesRestantes = nbPlacesRestantes;
+        this.listeReservations = new ArrayList<>();
+    }
 
     public int getNoRestaurant() {
         return noRestaurant;
@@ -43,14 +54,20 @@ public class restaurant implements Parcelable {
         this.nbPlacesRestantes = nbPlacesRestantes;
     }
 
-    public restaurant(int noRestaurant, String nomRestaurant, int nbPlacesMax, int nbPlacesRestantes) {
-        this.noRestaurant = noRestaurant;
-        this.nomRestaurant = nomRestaurant;
-        this.nbPlacesMax = nbPlacesMax;
-        this.nbPlacesRestantes = nbPlacesRestantes;
+    public ArrayList<reservation> getListeReservations() {
+        return listeReservations;
+    }
+
+    public void setListeReservations(ArrayList<reservation> listeReservations) {
+        this.listeReservations = listeReservations;
     }
 
     protected restaurant(Parcel in) {
+        noRestaurant = in.readInt();
+        nomRestaurant = in.readString();
+        nbPlacesMax = in.readInt();
+        nbPlacesRestantes = in.readInt();
+        listeReservations = in.createTypedArrayList(reservation.CREATOR);
     }
 
     public static final Creator<restaurant> CREATOR = new Creator<restaurant>() {
@@ -72,7 +89,11 @@ public class restaurant implements Parcelable {
 
     @Override
     public void writeToParcel(@NonNull Parcel dest, int flags) {
-
+        dest.writeInt(noRestaurant);
+        dest.writeString(nomRestaurant);
+        dest.writeInt(nbPlacesMax);
+        dest.writeInt(nbPlacesRestantes);
+        dest.writeTypedList(listeReservations);
     }
 
     @NonNull
